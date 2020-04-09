05980fbdd851f7fb8677b8ba555fbd3d7d506a58
Tanner Townsend <rsmcwow@gmail.com>
2020-04-09 02:15:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,15 +17,18 @@ class Triangle
   # -> 3, 4, -5
   # -> SUM of two sides are less than the greatest side
   def kind
+    if (@side1 <= 0 || @side2 <= 0 || @side3 <= 0)
+      raise TriangleError
+    elsif
 
-
-
-    if (@side1 == @side2) && (@side2 == @side3)
-       :equilateral
-    elsif (@side1 == @side2) || (@side1 == @side3) || (@side2 == @side3)
-       :isosceles
     else
-       :scalene
+      if (@side1 == @side2) && (@side2 == @side3)
+         :equilateral
+      elsif (@side1 == @side2) || (@side1 == @side3) || (@side2 == @side3)
+         :isosceles
+      else
+         :scalene
+      end
     end
   end
 
