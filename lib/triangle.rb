02f8997bd60e2e9bce58d514805dade180e9eb19
Tanner Townsend <rsmcwow@gmail.com>
2020-04-09 02:13:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,36 +1,29 @@
-require 'pry'
-
 class Triangle
-  def initalize(side1,side2,side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+  def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
   end
 
-  # Return the type of triangle
-  # -> Equilateral => All sides equal
-  # -> Isoceles    => Two sides same
-  # -> Scalene     => All side different
-
-  # ERROR IF:
-  # -> 0, 0, 0
-  # -> 3, 4, -5
-  # -> SUM of two sides are less than the greatest side
-  def kind
-    binding.pry
-
-    if (@side1 == @side2) && (@side2 == @side3)
-       :equilateral
-    elsif (@side1 == @side2) || (@side1 == @side3) || (@side2 == @side3)
-       :isosceles
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
     else
-       :scalene
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
     end
+
   end
 
-  class TriangleError < StandardError
-    def message
-      "ERROR: The Triangle violates the triangle inequality rules!"
-    end
-  end
+end
+
+class TriangleError < StandardError
+  # triangle error code
 end
