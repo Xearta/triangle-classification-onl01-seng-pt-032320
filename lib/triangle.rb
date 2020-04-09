d0c1efc583879922829282705a0ca1b83607bb7a
Tanner Townsend <rsmcwow@gmail.com>
2020-04-09 02:14:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,6 @@ class Triangle
   # -> 3, 4, -5
   # -> SUM of two sides are less than the greatest side
   def kind
-    binding.pry
 
     if (@side1 == @side2) && (@side2 == @side3)
        :equilateral
