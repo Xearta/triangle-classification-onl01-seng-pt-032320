a009a152b7219fc6cc282e2d98fcf03dc3ab0bf2
Tanner Townsend <rsmcwow@gmail.com>
2020-04-09 01:54:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
   attr_accessor :equilateral, :isosceles, :scalene, :length_1, :length_2, :length_3
+
+  def initalize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+
+  # Return the type of triangle
+  # -> Equilateral => All sides equal
+  # -> Isoceles    => Two sides same
+  # -> Scalene     => All side different
+  def kind
+    
+  end
 end
